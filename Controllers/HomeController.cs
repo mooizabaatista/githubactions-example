d754e6797e579701a1ea3b76c685a2e17bd26531
p-mbsilva <p-mbsilva@tce.sp.gov.br>
2025-04-23 08:29:05 -0300

    Iniciando o repo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using EShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Environment = _configuration["ambiente"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
